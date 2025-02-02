96a80f4f69897a963417585ee625264b4eb1ee78
SDDCA <castrum.dutra@gmail.com>
2025-02-02 00:57:39 -0300

    Second Commit Remaining Part Of CRUD

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Exo_WebApi
+# Exo_WebApi
--- obj/Debug/net9.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fdce0fbd764966dd25972751189d4535fcbb217")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
